d1a37f120c7c576065f11fbf3b6543c88336b905
haucknh <haucknh@mail.uc.edu>
2020-11-03 23:39:39 +0000

    save before run

--- Counter.java
+++ Counter.java
@@ -23,15 +23,18 @@ class Counter implements ActionListener {
 
       JButton left = new JButton("Left ");
       JButton right = new JButton("Right ");
+      JButton reset = new JButton("Reset ");
 
       left.addActionListener(this);
       right.addActionListener(this);
+      reset.addActionListener(this);
 
       frame.add(left);
       frame.add(right);
+      frame.add(reset, BorderLayout.SOUTH);
 
-      leftLabel = new JLabel("Left Count: " + leftCount);
-      rightLabel = new JLabel("Right Count " + rightCount);
+      leftLabel = new JLabel("Left Count: ");
+      rightLabel = new JLabel("Right Count: ");
       label1 = new JLabel("Press a button");
 
       frame.add(label1, BorderLayout.SOUTH);
@@ -43,10 +46,18 @@ class Counter implements ActionListener {
 
 public void actionPerformed(ActionEvent ae) {
   if(ae.getActionCommand().equals("Left ")){
+      leftCount ++;
       label1.setText("You pressed Left");
+      leftLabel.setText("Left Count: " + leftCount);
   }
-  else {
+  else if(ae.getActionCommand().equals("Right ")) {
+      rightCount ++;
       label1.setText("You pressed Right");
+      rightLabel.setText("Right Count: " + rightLabel);
+  }
+  else if(ae.getActionCommand().equals("Reset ")) {
+      leftCount = 0;
+      rightCount = 0;
   }
 }
 }
