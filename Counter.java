4c0aad4d06708d650045d62f630743d49ef00210
haucknh <haucknh@mail.uc.edu>
2020-11-03 23:28:59 +0000

    updateds

--- Counter.java
+++ Counter.java
@@ -4,11 +4,18 @@ import java.awt.*;
 
 class Counter implements ActionListener {
   JLabel label1;
+  JLabel rightLabel;
+  JLabel leftLabel;
+  int leftCount;
+  int rightCount;
 
   Counter(){
       JFrame frame = new JFrame("Let's count");
       frame.setLayout(new FlowLayout());
       frame.setSize(200,110);
+
+      leftCount = 0;
+      rightCount = 0;
 
       //when not commented out, below code will crash our IDE when GUI is exited 
       //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,8 +30,13 @@ class Counter implements ActionListener {
       frame.add(left);
       frame.add(right);
 
+      leftLabel = new JLabel("Left Count: " + leftCount);
+      rightLabel = new JLabel("Right Count " + rightCount);
       label1 = new JLabel("Press a button");
+
       frame.add(label1, BorderLayout.SOUTH);
+      frame.add(leftLabel);
+      frame.add(rightLabel);
       frame.setVisible(true);
 
   }
